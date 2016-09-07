ed70a9e22f4e446f3c7d7cdcf124be9e70cb2e0a
Maxim Urschumzew <u.maxim@live.de>
2016-09-07 13:36:49 +0200

    Add completion framework

---
+++ FatalRust/FatalRust.ProjectType/Completion/FRCompletionSourceProvider.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.ProjectSystem;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Operations;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FatalRust
+{
+    [Export(typeof(ICompletionSourceProvider))]
+    [ContentType("plaintext")]
+    [Name("token completion")]
+    [AppliesTo("FatalRust")]
+    internal class FRCompletionSourceProvider : ICompletionSourceProvider
+    {
+        [Import]
+        internal ITextStructureNavigatorSelectorService NavigatorService { get; set; }
+
+        public ICompletionSource TryCreateCompletionSource(ITextBuffer textBuffer)
+        {
+            return new FRCompletionSource(this, textBuffer);
+        }
+
+
+    }
+}
